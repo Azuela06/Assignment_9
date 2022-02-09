b8ef0d1a805a77066dd1bdf4bb7aa7c4ea66ff98
Azii <violetlovato05@gmail.com>
2022-02-09 18:16:47 +0800

    working on details

---
+++ details.py
@@ -0,0 +1,17 @@
+
+import json
+
+Personal_Info = {
+    "basic info":   [
+        {
+            "Fullname": "Julliane Maeve C. Azuela",
+            "Email" : "jmaeveazuela05@gmail.com",
+            "Address" : "General Hizon, Bangkal Makati City",
+            "Contact.no" : "+63 939 887 6828"
+        }
+    ],
+    "Education":    [
+        
+    ]   
+
+}
